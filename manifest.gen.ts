be6ae191b6896c324c29b008a32660ce070ad569
Luigi <luigidias@protonmail.com>
2024-07-08 15:18:08 -0300

    ...

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,9 +2,10 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/deco/wake.ts";
-import * as $$$$$$$$$$$1 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$2 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/deco/vtex.ts";
+import * as $$$$$$$$$$$1 from "./apps/deco/wake.ts";
+import * as $$$$$$$$$$$2 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$3 from "./apps/site.ts";
 import * as $$$2 from "./loaders/availableIcons.ts";
 import * as $$$3 from "./loaders/icons.ts";
 import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
@@ -126,9 +127,10 @@ const manifest = {
     "site/sections/Theme/Theme.tsx": $$$$$$51,
   },
   "apps": {
-    "site/apps/deco/wake.ts": $$$$$$$$$$$0,
-    "site/apps/decohub.ts": $$$$$$$$$$$1,
-    "site/apps/site.ts": $$$$$$$$$$$2,
+    "site/apps/deco/vtex.ts": $$$$$$$$$$$0,
+    "site/apps/deco/wake.ts": $$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$2,
+    "site/apps/site.ts": $$$$$$$$$$$3,
   },
   "name": "site",
   "baseUrl": import.meta.url,
